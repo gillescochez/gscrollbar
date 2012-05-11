8f3eb399e76e8cf90c4e0979580d40f8f74c1359
Gilles Cochez <git@csurl.eu>
2012-05-11 13:42:05 +0100

    some optimization + doc update

--- README.md
+++ README.md
@@ -1,15 +1,59 @@
-<<<<<<< HEAD
- *
- * Tiny Scrollbar 1.66 - http://www.baijs.nl/tinyscrollbar/
- * Copyright 2010, Maarten Baijs
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.opensource.org/licenses/gpl-2.0.php
- */
 
-=======
-gscrollbar
-==========
+# gscrollbar
 
-jquery custom scrollbar plugin with touch device support
->>>>>>> 381c72c8b7a58239123ee6f52375f54e62d3e408
+jquery custom scrollbar plugin with touch device support, lightweight and easy to use.
+
+# usage
+
+## initialize
+
+```javascript
+
+// can also pass an options setting as argument
+$('.scroll').gscrollbar();
+
+```
+## update
+
+To do an update simply call the plugin again and an initialized element and pass the string  update as a first argument.
+The second argument can be a number, an element or an anchor.
+
+```javascript
+
+$('#scroll1').gscrollbar('update', 100); // update with a given number
+
+```
+
+## options
+
+```javascript
+
+$.fn.gscrollbar.defaults = {
+    axis: 'y', // vertical or horizontal scrollbar? ( x || y ).
+    wheel: 40,  //how many pixels must the mouswheel scroll at a time.
+    scroll: true, //enable or disable the mousewheel;
+    size: 'auto', //set the size of the scrollbar to auto or a fixed number.
+    sizethumb: 'auto' //set the size of the thumb to auto or a fixed number.
+};
+
+```
+
+# credits
+
+gscrollbar is based on tiny scrollbar and it released under the same license.
+
+The differences between the 2 plugins  are the following:
+
+* Different API
+* gscrollbar inject the DOM it needs, tiny expects it (for every instance)
+* 
+
+So not much really, tiny scrollbar is really good I just didn't like having to set up the markup it expects in my documents for each element using it.
+
+## Tiny Scrollbar Copyright Notice
+
+Tiny Scrollbar 1.66 - http://www.baijs.nl/tinyscrollbar/
+Copyright 2010, Maarten Baijs
+Dual licensed under the MIT or GPL Version 2 licenses.
+http://www.opensource.org/licenses/mit-license.php
+http://www.opensource.org/licenses/gpl-2.0.php
--- js/gscrollbar.js
+++ js/gscrollbar.js
@@ -2,9 +2,15 @@
 (function($){
     
     function Scrollbar(root, options){
+
+            // help minification a bit
+            var min = Math.min;
+            var max = Math.max;
+            var $doc = $(document);
+
             var oSelf = this;
-            var oWrapper = root.addClass('gscrollbar').prepend('<div class="scrollbar"><div class="track"><div class="thumb"><div class="end"></div></div></div></div>');
-            var oViewport = { obj: $(root).children(':not(.scrollbar)').wrapAll('<div class="overview"></div>').end().children('.overview').wrap('<div class="viewport"></div>').parent().css('height', root.height()) };
+            var oWrapper = root.addClass('gscrollbar').prepend('<div class="scrollbar"><div class="track"><div class="thumb"></div></div></div>');
+            var oViewport = { obj: $(root).children(':not(.scrollbar)').wrapAll('<div class="overview" />').end().children('.overview').wrap('<div class="viewport" />').parent().css('height', root.height()) };
             var oContent = { obj: oViewport.obj.children(0) };
             var oScrollbar = { obj: $('.scrollbar', root) };
             var oTrack = { obj: $('.track', oScrollbar.obj) };
@@ -23,9 +29,9 @@
                 oContent.ratio = oViewport[options.axis] / oContent[options.axis];
                 oScrollbar.obj.toggleClass('disable', oContent.ratio >= 1);
                 oTrack[options.axis] = options.size == 'auto' ? oViewport[options.axis] : options.size;
-                oThumb[options.axis] = Math.min(oTrack[options.axis], Math.max(0, ( options.sizethumb == 'auto' ? (oTrack[options.axis] * oContent.ratio) : options.sizethumb )));
+                oThumb[options.axis] = min(oTrack[options.axis], max(0, ( options.sizethumb == 'auto' ? (oTrack[options.axis] * oContent.ratio) : options.sizethumb )));
                 oScrollbar.ratio = options.sizethumb == 'auto' ? (oContent[options.axis] / oTrack[options.axis]) : (oContent[options.axis] - oViewport[options.axis]) / (oTrack[options.axis] - oThumb[options.axis]);
-                iScroll = (sScroll == 'relative' && oContent.ratio <= 1) ? Math.min((oContent[options.axis] - oViewport[options.axis]), Math.max(0, iScroll)) : 0;
+                iScroll = (sScroll == 'relative' && oContent.ratio <= 1) ? min((oContent[options.axis] - oViewport[options.axis]), max(0, iScroll)) : 0;
                 iScroll = (sScroll == 'bottom' && oContent.ratio <= 1) ? (oContent[options.axis] - oViewport[options.axis]) : isNaN(parseInt(sScroll)) ? iScroll : parseInt(sScroll);
                 setSize();
             };
@@ -59,15 +65,15 @@
                 iMouse.start = sAxis ? oEvent.pageX : oEvent.pageY;
                 var oThumbDir = parseInt(oThumb.obj.css(sDirection));
                 iPosition.start = oThumbDir == 'auto' ? 0 : oThumbDir;
-                $(document).bind('mousemove', drag);
-                 document.ontouchmove = function(oEvent){
-                    $(document).unbind('mousemove');
+                $doc.bind('mousemove', drag);
+                 $doc[0].ontouchmove = function(oEvent){
+                    $doc.unbind('mousemove');
                     drag(oEvent.touches[0]);
                 };
-                $(document).bind('mouseup', end);
+                $doc.bind('mouseup', end);
                 oThumb.obj.bind('mouseup', end);
-                 oThumb.obj[0].ontouchend = document.ontouchend = function(oEvent){
-                    $(document).unbind('mouseup');
+                 oThumb.obj[0].ontouchend = $doc[0].ontouchend = function(oEvent){
+                    $doc.unbind('mouseup');
                     oThumb.obj.unbind('mouseup');
                     end(oEvent.touches[0]);
                 };
@@ -79,7 +85,7 @@
                     var oEvent = oEvent || window.event;
                     var iDelta = oEvent.wheelDelta ? oEvent.wheelDelta/120 : -oEvent.detail/3;
                     iScroll -= iDelta * options.wheel;
-                    iScroll = Math.min((oContent[options.axis] - oViewport[options.axis]), Math.max(0, iScroll));
+                    iScroll = min((oContent[options.axis] - oViewport[options.axis]), max(0, iScroll));
                     oThumb.obj.css(sDirection, iScroll / oScrollbar.ratio);
                     oContent.obj.css(sDirection, -iScroll);
                     
@@ -89,16 +95,16 @@
             };
 
             function end(oEvent){
-                $(document).unbind('mousemove', drag);
-                $(document).unbind('mouseup', end);
+                $doc.unbind('mousemove', drag);
+                $doc.unbind('mouseup', end);
                 oThumb.obj.unbind('mouseup', end);
-                document.ontouchmove = oThumb.obj[0].ontouchend = document.ontouchend = null;
+                $doc[0].ontouchmove = oThumb.obj[0].ontouchend = $doc[0].ontouchend = null;
                 return false;
             };
 
             function drag(oEvent){
                 if(!(oContent.ratio >= 1)){
-                    iPosition.now = Math.min((oTrack[options.axis] - oThumb[options.axis]), Math.max(0, (iPosition.start + ((sAxis ? oEvent.pageX : oEvent.pageY) - iMouse.start))));
+                    iPosition.now = min((oTrack[options.axis] - oThumb[options.axis]), max(0, (iPosition.start + ((sAxis ? oEvent.pageX : oEvent.pageY) - iMouse.start))));
                     iScroll = iPosition.now * oScrollbar.ratio;
                     oContent.obj.css(sDirection, -iScroll);
                     oThumb.obj.css(sDirection, iPosition.now);
